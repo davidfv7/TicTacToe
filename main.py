e0bc6178224b67da0c5fb829933987c794125702
davidfv7 <davidxflo@gmail.com>
2019-09-25 19:50:15 -0500

    Creacion de main

---
+++ main.py
@@ -0,0 +1,10 @@
+play = True
+while(play):
+    print("Instrucciones:")
+    
+
+    print("¿Otra partida?")
+    another_one= input()
+    N
+    if(another_one.lower()=="N".lower()):
+        play=False
