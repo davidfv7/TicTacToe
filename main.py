d0961be81b36986726035e77c9386ec77e21f0f1
JAQUILES <jantaigr@gmail.com>
2019-09-25 21:50:33 -0500

    Se agregan Instrucciones

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
 play = True
 while(play):
-    print("Instrucciones:")
-    
+    print(".................JUEGO DEL GATO...........................\n"
+          "Instrucciones:                                            \n"
+          "   1. Observa la siguiente matriz                         \n"
+          "             A     B      C                               \n"
+          "                |     |                                   \n"
+          "        0   ____|_____|____                               \n"
+          "                |     |                                   \n"
+          "        1   ____|_____|____                               \n"
+          "                |     |                                   \n"
+          "        2       |     |                                   \n"
+          "                                                          \n"
+          "   2. Coloca la posición de tu jugada mediante coordenadas\n"
+          "      como (A,0) o (C,2)                                  \n"
+          "                                                          \n")
+    print("-------------::::COMIENZA EL JUEGO::::--------------------\n")
 
     print("¿Otra partida?")
     another_one= input()
